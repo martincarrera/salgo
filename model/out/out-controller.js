2398373316a7ff78a8f685a75286830e2cf33935
Martin Carrera <mcarrera.92@gmail.com>
2017-03-03 18:02:33 -0300

    Add placeholder for out order

--- model/out/out-controller.js
+++ model/out/out-controller.js
@@ -1,6 +1,18 @@
 const Controller = require('../../lib/controller');
 const outFacade = require('./out-facade');
 
-class OutController extends Controller {}
+class OutController extends Controller {
+
+  createFromSlack(req, res, next) {
+    this.facade.createFromSlack(req.body)
+      .then((doc) => {
+        res.status(201).json(doc);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+
+}
 
 module.exports = new OutController(outFacade);
--- model/out/out-facade.js
+++ model/out/out-facade.js
@@ -1,6 +1,15 @@
 const Model = require('../../lib/facade');
 const outSchema = require('./out-schema');
 
-class OutModel extends Model {}
+class OutModel extends Model {
+
+  static createFromSlack(input) {
+    console.log(input); // eslint-disable-line no-console
+    return Promise.resolve({
+      text: ':speaking_head_in_silhouette: Someone is going out! :runner: \nLet them know what you want with `/order [item]`.',
+    });
+  }
+
+}
 
 module.exports = new OutModel(outSchema);
