d142be6112e6798c8b15dedf6192f2b2069dbb33
Martin Carrera <mcarrera.92@gmail.com>
2017-04-05 15:17:55 -0300

    Minor text upgrade

--- model/back/back-facade.js
+++ model/back/back-facade.js
@@ -28,20 +28,20 @@ class BackModel extends Model {
     .then(([doc]) => {
       if (doc) {
         if (doc.orders.length > 0) {
-          text = 'Hey!';
+          text = 'Hey! ';
           const usersToNotify = Array.from(
             new Set(doc.orders.map(element => element.createdBy.user.name)));
           usersToNotify.forEach((user) => {
-            text = `${text} @${user}, `;
+            text = `${text}@${user}, `;
           });
-          text = `${text}hurry up!`;
-        }
-        text = `${text} @${doc.createdBy.user.name} is back!`;
-        if (doc.orders.length > 0) {
+          text = `${text}hurry up! @${doc.createdBy.user.name} is back!`;
+
           text = `${text} The final list was:`;
           doc.orders.forEach((element, index) => {
             text = `${text} \n ${index + 1}. *${element.text}* for ${element.createdBy.user.name}`;
           });
+        } else {
+          text = `${text} @${doc.createdBy.user.name} is back!`;
         }
       } else {
         text = 'Hey! Nobody was out! Are you going out? Just type `/out` to let everybody know!';
