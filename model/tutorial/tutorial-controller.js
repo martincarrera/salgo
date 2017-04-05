3d2020dada3715befa9031b72551f1d9b9353df3
Martin Carrera <mcarrera.92@gmail.com>
2017-04-05 15:23:31 -0300

    Fix tutorial bug

--- model/tutorial/tutorial-controller.js
+++ model/tutorial/tutorial-controller.js
@@ -1,6 +1,12 @@
 const Controller = require('../../lib/controller');
 const tutorialFacade = require('./tutorial-facade');
 
-class TutorialController extends Controller {}
+class TutorialController extends Controller {
+  showTutorial(req, res, next) {
+    this.facade.showTutorial(req.body)
+      .then(doc => res.status(201).json(doc))
+      .catch(err => next(err));
+  }
+}
 
 module.exports = new TutorialController(tutorialFacade);
